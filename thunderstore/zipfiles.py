69177486f1ba5072ecf3d9da87cac60e0b8dc4a1
Chev <11602755+chev2@users.noreply.github.com>
2020-08-14 20:26:27 -0700

    Zipfiles.py

---
+++ thunderstore/zipfiles.py
@@ -0,0 +1,13 @@
+import os
+import zipfile
+
+def zipfiles(z_obj, dir):
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            file_loc = file if not file.endswith('.dll') else f'plugins/{file}'
+            z_obj.write(os.path.join(root, file), arcname=file_loc)
+
+for folder in next(os.walk('.'))[1]:
+    print(f'{folder}/')
+    with zipfile.ZipFile(f'{folder}.zip', 'w') as z:
+        zipfiles(z, f'{folder}/')
