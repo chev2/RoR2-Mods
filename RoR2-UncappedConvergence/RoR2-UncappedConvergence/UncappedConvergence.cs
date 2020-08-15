1d7e7b6621e1323796fb7d392cb0348b1c02bae5
Chev <11602755+chev2@users.noreply.github.com>
2020-08-14 19:11:15 -0700

    RoR2-UncappedConvergence Initial Commit

---
+++ RoR2-UncappedConvergence/RoR2-UncappedConvergence/UncappedConvergence.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using UnityEngine;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System.Runtime.CompilerServices;
+using R2API.Utils;
+
+namespace ChevRoR
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.ChevRoR.UncappedConvergence", "Uncapped Convergence", "1.0.0")]
+    public class UncappedConvergence : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            IL.RoR2.HoldoutZoneController.FocusConvergenceController.FixedUpdate += (il) =>
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(
+                    x => x.MatchCall<UnityEngine.Mathf>("Min")
+                );
+                c.Index -= 3;
+                c.RemoveRange(6); // remove Mathf.min code
+            };
+
+            Console.print("\'Uncapped Convergence\' mod loaded");
+        }
+    }
+}
