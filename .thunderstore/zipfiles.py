793646dfe476ddb798a49d5e4347759ff6182330
Chev <11602755+chev2@users.noreply.github.com>
2021-04-18 17:55:10 -0700

    Update zipfiles.py to include author folder

--- .thunderstore/zipfiles.py
+++ .thunderstore/zipfiles.py
@@ -4,7 +4,7 @@ import zipfile
 def zipfiles(z_obj, dir):
     for root, dirs, files in os.walk(dir):
         for file in files:
-            file_loc = file if not file.endswith('.dll') else f'plugins/{file}'
+            file_loc = file if not file.endswith('.dll') else f'plugins/Chev/{file}'
             z_obj.write(os.path.join(root, file), arcname=file_loc)
 
 for folder in next(os.walk('.'))[1]:
