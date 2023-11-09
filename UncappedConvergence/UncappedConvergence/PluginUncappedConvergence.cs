cfbe00be8b04f40dec0b3d29943e39e21821979a
Chev <riskyrains@proton.me>
2023-11-08 16:55:05 -0800

    UncappedConvergence: Update to latest game version

---
+++ UncappedConvergence/UncappedConvergence/PluginUncappedConvergence.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using MonoMod.Cil;
+using System.Security.Permissions;
+
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+namespace Chev
+{
+    [BepInDependency(R2API.R2API.PluginGUID)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class PluginUncappedConvergence : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "Chev";
+        public const string PluginName = "UncappedConvergence";
+        public const string PluginVersion = "2.0.0";
+
+        public void Awake()
+        {
+            Logger.LogInfo("Hooking into HoldoutZoneController.FocusConvergenceController.FixedUpdate...");
+
+            IL.RoR2.HoldoutZoneController.FocusConvergenceController.FixedUpdate += (il) =>
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(
+                    x => x.MatchCall<UnityEngine.Mathf>("Min")
+                );
+                c.Index -= 3;
+                c.RemoveRange(6); // remove Mathf.min code
+            };
+
+            Logger.LogInfo("Hook finished, mod loaded");
+        }
+    }
+}
